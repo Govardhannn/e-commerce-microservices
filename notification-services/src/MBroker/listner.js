964467287a85c924316a3d860309961063d1587b
Govardhannn <sahanigovardhan69@gmail.com>
2026-02-13 14:44:59 +0000

    feat(notification): consume user-created event and send welcome email

--- notification-services/src/MBroker/listner.js
+++ notification-services/src/MBroker/listner.js
@@ -1,14 +1,18 @@
 import { subscribeToQueue } from "./broker.js";
+import sendEmail from "../email.js";
+
+export default function () {
+  subscribeToQueue("AUTH_NOTIFICATION.USER_CREATED", async (data) => {
+    console.log("Reseved the data from the Queue", data);
 
 
-export default function (){
-
-     subscribeToQueue  ("AUTH_NOTIFICATION.USER_CREATED", async (data) => {
-  console.log("Reseved the data from the Queue", data);
-});
-
-
+    // here sending mail with user data
+    const emailHTMLTemplate = `
+        <h1>Welcome to Our Service!</h1>
+        <p>Dear ${data.fullName.firstName + " " + (data.fullName.lastName || "")},</p>
+        <p>Thank you for registering with us. We're excited to have you on board!</p>
+        <p>Best regards,<br/>The Team</p>
+        `;
+         await sendEmail(data.email, "Welcome to Our Service", "Thank you for registering with us!", emailHTMLTemplate);
+  });
 }
-
-
-
