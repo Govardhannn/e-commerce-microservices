a6edde0503df72421208fd14c8d2323d64a4fcc5
Govardhannn <sahanigovardhan69@gmail.com>
2026-02-13 14:37:12 +0000

    feat(notification-service) testing that data is getting from auth-service to notificaion service it's working

--- notification-services/server.js
+++ notification-services/server.js
@@ -4,7 +4,8 @@ import app from "./src/app.js";
 
 import { connect } from "./src/MBroker/broker.js";
  
-
+// importing form the listner.js
+import appLister from "./src/MBroker/listner.js"
 
 const port = process.env.PORT || 8007;
 
@@ -15,7 +16,7 @@ const port = process.env.PORT || 8007;
 const connectionString = async () => {
   await connect();
 
-
+ appLister()
   app.listen(port, () => {
     console.log(`Notification service is running on port${port}`);
   });
--- notification-services/src/MBroker/listner.js
+++ notification-services/src/MBroker/listner.js
@@ -1,4 +1,14 @@
+import { subscribeToQueue } from "./broker.js";
 
+
+export default function (){
+
+     subscribeToQueue  ("AUTH_NOTIFICATION.USER_CREATED", async (data) => {
+  console.log("Reseved the data from the Queue", data);
+});
+
+
+}
 
 
 
